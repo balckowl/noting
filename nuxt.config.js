e8d170add404a110f09e7d8eda8c5b4197ad5c22
窪山天望 <shiggyjr1492@gmail.com>
2023-07-21 02:06:34 +0900

    Add: プリズムのプラグインのパスを通す

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/filter.js'],
+  plugins: ['~/plugins/filter.js', '~/plugins/prism.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,

1fd9c3436842bf71183d0f041a77262b4b97ddf5
窪山天望 <shiggyjr1492@gmail.com>
2023-07-21 19:24:43 +0900

    update: OGPの設定部分を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
   head: {
     title: 'noting',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
     },
     meta: [
       { charset: 'utf-8' },
@@ -25,7 +26,14 @@ export default {
     //google fonts
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
     { rel: "preconnect", href: "https://fonts.gstatic.com" },
-    { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Agdasima&display=swap" }
+    { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Agdasima&display=swap" },
+    { hid: 'og:site_name', property: 'og:site_name', content: 'サイト名' },
+    { hid: 'og:type', property: 'og:type', content: 'website' },
+    { hid: 'og:url', property: 'og:url', content: 'サイトのURL' },
+    { hid: 'og:title', property: 'og:title', content: 'サイトタイトル' },
+    { hid: 'og:description', property: 'og:description', content: '共通ディスクリプション' },
+    { hid: 'og:image', property: 'og:image', content: '画像のURL' },
+    { name: 'twitter:card', content: 'summary' }//　twitterの画像サイズ
     ],
     script: [
       { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' }

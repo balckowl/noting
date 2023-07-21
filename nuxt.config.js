13b956a18260f6912764f3e2d754e1a460e28e53
窪山天望 <shiggyjr1492@gmail.com>
2023-07-22 00:14:43 +0900

    Update: twitterのOGP画像を大きいものに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
     { hid: 'og:title', property: 'og:title', content: 'サイトタイトル' },
     { hid: 'og:description', property: 'og:description', content: '共通ディスクリプション' },
     { hid: 'og:image', property: 'og:image', content: '画像のURL' },
-    { name: 'twitter:card', content: 'summary' }//　twitterの画像サイズ
+    { name: 'twitter:card', content:'summary_large_image' }//　twitterの画像サイズ
     ],
     script: [
       { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' }

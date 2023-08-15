3eefe43b9018cc380660a595efa9701c2e79a11c
窪山天望 <shiggyjr1492@gmail.com>
2023-08-15 21:10:56 +0900

    Update: blog, workフォルダを修正

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -6,23 +6,31 @@
       </base-heading>
       <layout-card-list-wrapper>
         <layout-card-list name="blog" v-for="(item, index) in items" :id="item.id" :key="index" :date="item.date"
-          :image="item.image" :image-url="item.image.url" :body="item.body" :title="item.title" :isShadow="true"/>
+          :image="item.image" :image-url="item.image.url" :body="item.body" :title="item.title" :isShadow="true" />
       </layout-card-list-wrapper>
+      <BasePagination :pager="pager" :current="Number(page)" />
     </div>
   </section>
 </template>
 
 <script>
 import axios from 'axios'
+import BasePagination from '../../components/BasePagination.vue'
 
 export default {
-  async asyncData({ $config }) {
-    const { data } = await axios.get(`${$config.apiUrl}/blog?limit=20`, {
+  async asyncData({ $config, params }) {
+    const page = params.p || '1'
+    const limit = 8
+
+    const { data } = await axios.get(`${$config.apiUrl}/blog?limit=${limit}&offset=${(page - 1) * limit}`, {
       headers: { 'X-API-KEY': $config.apiKey },
-    })
+    });
     return {
       items: data.contents,
-    }
+      page,
+      pager: [...Array(Math.ceil(data.totalCount / limit)).keys()],
+    };
   },
+  components: { BasePagination }
 }
 </script>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -8,22 +8,24 @@
         <layout-card-list name="work" v-for="(item, index) in items" :id="item.id" :key="index" :date="item.date"
           :image="item.image" :image-url="item.image.url" :title="item.title" :body="item.body" :isShadow="true" />
       </layout-card-list-wrapper>
+      <BasePagination page="work" :pager="pager" :current="Number(page)"/>
     </div>
   </section>
 </template>
 
 <script>
 import axios from 'axios'
+import BasePagination from '../../components/BasePagination.vue';
 export default {
-  async asyncData({ $config }) {
-
-    const { data } = await axios.get(`${$config.apiUrl}/work`, {
-      headers: { 'X-API-KEY': $config.apiKey },
-    })
-    return {
-      items: data.contents,
-    }
-  }
+    async asyncData({ $config }) {
+        const { data } = await axios.get(`${$config.apiUrl}/work`, {
+            headers: { 'X-API-KEY': $config.apiKey },
+        });
+        return {
+            items: data.contents,
+        };
+    },
+    components: { BasePagination }
 } 
 </script>
 

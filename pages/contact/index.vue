c5d55a356dfbf7a82cebcccbf825401910163430
窪山天望 <shiggyjr1492@gmail.com>
2023-08-17 01:20:13 +0900

    Update: iframeでサイトにお問い合わせフォームを埋め込む

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,21 +1,7 @@
 <template>
-    <div class="container">
-        <h1>お問い合わせ</h1>
-        <form name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
-            <div>
-                <label for="name">お名前（必須）</label>
-                <input name="name" type="text" required />
-            </div>
-            <div>
-                <label for="email">メールアドレス（必須）</label>
-                <input name="email" type="email" required />
-            </div>
-            <div>
-                <label for="content">お問い合わせ内容</label>
-                <textarea name="content" rows="10" required></textarea>
-            </div>
-            <button type="submit">送信する</button>
-        </form>
+    <div>
+        <script src="https://sdk.form.run/js/v2/embed.js"></script>
+        <div class="formrun-embed" data-formrun-form="@noting" data-formrun-redirect="true">
+        </div>
     </div>
 </template>

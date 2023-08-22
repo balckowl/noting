e15f9700be26d30d315a88786888e9dc08825661
窪山天望 <shiggyjr1492@gmail.com>
2023-08-22 18:03:16 +0900

    Update: toolbarのcssを追加しました。

--- plugins/prism.js
+++ plugins/prism.js
@@ -2,6 +2,7 @@ import Prism from 'prismjs'
 
 import 'prismjs/themes/prism-tomorrow.css'
 import 'prismjs/plugins/toolbar/prism-toolbar'
+import 'prismjs/plugins/toolbar/prism-toolbar.min.css'
 
 import 'prismjs/plugins/show-language/prism-show-language'
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'

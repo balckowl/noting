0ab0472f8c67f25c69039da38431459b5ae10330
窪山天望 <shiggyjr1492@gmail.com>
2023-08-22 17:11:17 +0900

    Update: importする順番を変更しました。

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,11 +1,13 @@
 import Prism from 'prismjs'
 
-import 'prismjs/themes/prism-okaidia.css'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/plugins/toolbar/prism-toolbar'
+
+import 'prismjs/plugins/show-language/prism-show-language'
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
 
 import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
-import 'prismjs/plugins/toolbar/prism-toolbar.min.css'
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js'
 
 export default Prism
